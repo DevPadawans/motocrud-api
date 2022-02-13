f82dd283d6ecedbc54a574b502735710470ac511
Handerson Frota <servicozuptecnologia741295@userede.com.br>
2022-02-13 17:33:37 -0300

    teste_initarios - criando o primeiro teste

--- src/test/java/com/devpadawans/motocrudapi/MotocrudApiApplicationTests.java
+++ src/test/java/com/devpadawans/motocrudapi/MotocrudApiApplicationTests.java
@@ -1,10 +1,78 @@
 package com.devpadawans.motocrudapi;
 
+import com.devpadawans.motocrudapi.commons.utils.PathUtils;
+import com.devpadawans.motocrudapi.model.Membro;
+import com.devpadawans.motocrudapi.repository.MembroRepository;
+import com.devpadawans.motocrudapi.service.MembroService;
+import com.devpadawans.motocrudapi.service.implmentation.MembroServiceImpl;
+import com.devpadawans.motocrudapi.web.MembroController;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 class MotocrudApiApplicationTests {
 
+    protected MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+
+    @Before
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+    @Test
+    public void deveriaPegarUmaListaDeMembros() throws Exception {
+        String uri = PathUtils.RESOURCE_SEARCH;
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        Membro[] membrosList = this.mapFromJson(content, Membro[].class);
+        assertTrue(membrosList.length > 0);
+    }
 
 }
